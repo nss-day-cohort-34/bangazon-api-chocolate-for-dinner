b327cb4e1c3bc3be1e28cfb40525f3e373c301e1
Brantley Jones <brantleyjones74@gmail.com>
2019-11-07 11:16:33 -0600

    commit

--- BangazonAPI/Controllers/TrainingProgramsController.cs
+++ BangazonAPI/Controllers/TrainingProgramsController.cs
@@ -63,6 +63,7 @@ namespace BangazonAPI.Controllers
                             Name = reader.GetString(reader.GetOrdinal("Name")),
                             StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
                             EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
+                            MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees"))
                         };
                         TrainingPrograms.Add(newtrainingProgram);
                     }
@@ -74,134 +75,80 @@ namespace BangazonAPI.Controllers
             }
         }
 
-        //// GET api/trainingPrograms/5
-        //[HttpGet("{id}", Name = "GettrainingProgram")]
-        //public async Task<IActionResult> Get(int id, string _include, string q)
-        //{
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            if (_include == "products")
-        //            {
-        //                cmd.CommandText = @"SELECT c.Id AS trainingProgramId, c.FirstName, c.LastName, c.CreationDate, c.LastActiveDate,
-        //                                                               p.Id AS ProductId, p.Title, p.Price, p.ProductTypeId, p.Description, p.Quantity,
-        //                                                               pt.Name AS ProductType
-        //                                                    FROM trainingProgram c
-        //                                                               LEFT JOIN Product p ON c.Id = p.trainingProgramId
-        //                                                               INNER JOIN ProductType pt ON pt.Id = p.ProductTypeId
-        //                                                    WHERE c.Id = @Id";
-        //            }
+        // GET api/trainingPrograms/1
+        [HttpGet("{id}", Name = "GettrainingProgram")]
+        public async Task<IActionResult> Get(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+               
+                    cmd.CommandText = @"SELECT Id, Name, StartDate, EndDate, MaxAttendees
+                                          FROM TrainingProgram
+                                         WHERE Id = @Id";
 
-        //            else if (_include == "payments")
-        //            {
-        //                cmd.CommandText = @"SELECT c.Id AS trainingProgramId, c.FirstName, c.LastName, c.CreationDate, c.LastActiveDate,
-        //                                                                pyt.Id AS PaymentTypeId, pyt.AcctNumber, pyt.Name AS PaymentTypeName
-        //                                                FROM trainingProgram c
-        //                                                                LEFT JOIN PaymentType pyt ON c.Id = pyt.trainingProgramId
-        //                                                    WHERE c.Id = @Id";
-        //            }
+                    cmd.Parameters.Add(new SqlParameter("@Id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
 
-        //            else if (q != null)
-        //            {
-        //                cmd.CommandText = @"SELECT Id AS trainingProgramId, FirstName, LastName, CreationDate, LastActiveDate
-        //                                                        FROM trainingProgram
-        //                                                        WHERE Id = @Id AND (FirstName LIKE @q OR LastName LIKE @q)";
-        //                cmd.Parameters.Add(new SqlParameter("@q", $"%{q}%"));
-        //            }
+                    TrainingProgram selectedtrainingProgram = null;
 
-        //            else
-        //            {
-        //                cmd.CommandText = @"SELECT Id AS trainingProgramId, FirstName, LastName, CreationDate, LastActiveDate
-        //                                                        FROM trainingProgram
-        //                                                        WHERE Id = @Id";
-        //            }
+                    while (reader.Read())
+                    {
+                        if (selectedtrainingProgram == null)
+                        {
+                            selectedtrainingProgram = new TrainingProgram
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
+                                EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
+                                MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees"))
+                            };
+                        }
 
-        //            cmd.Parameters.Add(new SqlParameter("@Id", id));
-        //            SqlDataReader reader = await cmd.ExecuteReaderAsync();
-
-        //            trainingProgram selectedtrainingProgram = null;
-
-        //            while (reader.Read())
-        //            {
-        //                if (selectedtrainingProgram == null)
-        //                {
-        //                    selectedtrainingProgram = new trainingProgram
-        //                    {
-        //                        Id = reader.GetInt32(reader.GetOrdinal("trainingProgramId")),
-        //                        FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-        //                        LastName = reader.GetString(reader.GetOrdinal("LastName")),
-        //                        CreationDate = reader.GetDateTime(reader.GetOrdinal("CreationDate")),
-        //                        LastActiveDate = reader.GetDateTime(reader.GetOrdinal("LastActiveDate"))
-        //                    };
-        //                }
-
-        //                if (_include == "products")
-        //                {
-        //                    if (!reader.IsDBNull(reader.GetOrdinal("ProductId")))
-        //                    {
-        //                        Product newProduct = new Product
-        //                        {
-        //                            Id = reader.GetInt32(reader.GetOrdinal("ProductId")),
-        //                            ProductTypeId = reader.GetInt32(reader.GetOrdinal("ProductTypeId")),
-        //                            Price = reader.GetDecimal(reader.GetOrdinal("Price")),
-        //                            Title = reader.GetString(reader.GetOrdinal("Title")),
-        //                            Description = reader.GetString(reader.GetOrdinal("Description")),
-        //                            Quantity = reader.GetInt32(reader.GetOrdinal("Quantity"))
-        //                        };
-        //                        selectedtrainingProgram.Products.Add(newProduct);
-        //                    }
-        //                }
-
-        //                else if (_include == "payments")
-        //                {
-        //                    if (!reader.IsDBNull(reader.GetOrdinal("PaymentTypeId")))
-        //                    {
-        //                        PaymentType newPayment = new PaymentType
-        //                        {
-        //                            Id = reader.GetInt32(reader.GetOrdinal("PaymentTypeId")),
-        //                            AcctNumber = reader.GetString(reader.GetOrdinal("AcctNumber")),
-        //                            Name = reader.GetString(reader.GetOrdinal("PaymentTypeName"))
-        //                        };
-        //                        selectedtrainingProgram.PaymentTypes.Add(newPayment);
-        //                    }
-        //                }
-        //            }
-        //            reader.Close();
-        //            return Ok(selectedtrainingProgram);
-        //        }
-        //    }
-        //}
+                    }
+                    reader.Close();
+                    return Ok(selectedtrainingProgram);
+                }
+            }
+        }
 
         //// POST api/trainingPrograms
-        //[HttpPost]
-        //public async Task<IActionResult> Post([FromBody] trainingProgram trainingProgram)
-        //{
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            // More string interpolation
-        //            cmd.CommandText = @"
-        //                INSERT INTO trainingProgram (FirstName, LastName, CreationDate, LastActiveDate)
-        //                OUTPUT INSERTED.Id
-        //                VALUES (@FirstName, @LastName, @CreationDate, @LastActiveDate)
-        //            ";
-        //            cmd.Parameters.Add(new SqlParameter("@FirstName", trainingProgram.FirstName));
-        //            cmd.Parameters.Add(new SqlParameter("@LastName", trainingProgram.LastName));
-        //            cmd.Parameters.Add(new SqlParameter("@CreationDate", trainingProgram.CreationDate));
-        //            cmd.Parameters.Add(new SqlParameter("@LastActiveDate", trainingProgram.LastActiveDate));
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TrainingProgram trainingProgram)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO TrainingProgram (
+                                    Name, 
+                                    StartDate, 
+                                    EndDate, 
+                                    MaxAttendees,
+                                    IsDeleted
+                                    )
+                        OUTPUT INSERTED.Id
+                        VALUES (@Name, @StartDate, @EndDate, @MaxAttendees, @IsDeleted)
+                    ";
+                    cmd.Parameters.Add(new SqlParameter("@Name", trainingProgram.Name));
+                    cmd.Parameters.Add(new SqlParameter("@StartDate", trainingProgram.StartDate));
+                    cmd.Parameters.Add(new SqlParameter("@EndDate", trainingProgram.EndDate));
+                    cmd.Parameters.Add(new SqlParameter("@MaxAttendees", trainingProgram.MaxAttendees));
+                    cmd.Parameters.Add(new SqlParameter("@IsDeleted", trainingProgram.IsDeleted));
 
-        //            trainingProgram.Id = (int)await cmd.ExecuteScalarAsync();
+                    trainingProgram.Id = (int)await cmd.ExecuteScalarAsync();
 
-        //            return CreatedAtRoute("GettrainingProgram", new { id = trainingProgram.Id }, trainingProgram);
-        //        }
-        //    }
-        //}
+                    return CreatedAtRoute("GettrainingProgram", new { id = trainingProgram.Id }, trainingProgram);
+                }
+            }
+        }
 
-        //// PUT api/trainingPrograms/5
+        // PUT api/trainingPrograms/5
         //[HttpPut("{id}")]
         //public async Task<IActionResult> Put([FromRoute] int id, [FromBody] trainingProgram trainingProgram)
         //{
